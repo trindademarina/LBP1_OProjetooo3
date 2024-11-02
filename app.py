68388a7275b85a9ea02adafdb6988b445f73c83a
trindademarina <135060656+trindademarina@users.noreply.github.com>
2024-11-01 23:08:58 -0300

    nsei

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-from flask import Flask, render_template
-from controllers.controller import projeto
 
-app = Flask(__name__)
-app.secret_key = 'batataCmKetchup'
-app.register_blueprint(projeto)
-
-
-if __name__  == '__main__':
-    app.run(debug=True)
